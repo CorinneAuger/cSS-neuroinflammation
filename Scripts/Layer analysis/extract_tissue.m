7711a8611506f34cf7fb51d70be6b51fa1c68188
CorinneAuger <corinneauger7@gmail.com>
2022-11-18 16:29:49 -0500

    Comment

--- Scripts/Layer analysis/extract_tissue.m
+++ Scripts/Layer analysis/extract_tissue.m
@@ -1,18 +1,19 @@
 function [tissue_mask, tissue_map_mask_light] = extract_tissue(image, cortex_mask, stain)
 
 %% Input directories
-directory.original_images = 
-directory.scripts = 
+directory.original_images = '/Volumes/Corinne hard drive/cSS project/Original images';
+directory.scripts = '/Volumes/Corinne hard drive/cSS project/Scripts/Layer analysis';
 
-cd '/Users/corinneauger/Documents/Aiforia heatmap coregistration/Original images'
-%image = original_image;
-%original_green = image(:,:,2);
+cd(directory.original_images)
+
+%% Set up input image
 original_green = image;
-
 [channel_x, channel_y, ~] = size(original_green);
 
+%% Preallocate
 tissue_map_mask_light = NaN([channel_x, channel_y, 1]);
 
+%% Set mask thresholds
 if strcmp(stain, 'GFAP') == 1
     cutoff = 200;
 elseif strcmp(stain, 'Iron') == 1
@@ -21,6 +22,7 @@ elseif strcmp(stain, 'CD68') == 1
     cutoff = 230;
 end
 
+%% Make masks
 for i = 1:channel_x
     for j = 1:channel_y
         if original_green(i, j) <= 15
@@ -33,10 +35,12 @@ for i = 1:channel_x
     end
 end
 
+%% Median filtering, filling small gaps
 se = strel('square', 150);
 dilated_tissue_map_mask_light = ~imdilate(1-medfilt2(tissue_map_mask_light, [15,15]),se);
 filled = imfill(dilated_tissue_map_mask_light, 'holes');
 
+%% Make sure everything in the cortex mask from the density comparison is also in the median filtered mask
 for i = 1:channel_x
     for j = 1:channel_y
         if cortex_mask(i,j) == 1 
@@ -45,8 +49,13 @@ for i = 1:channel_x
     end
 end
 
+%% Median filter again
 filtered_tissue_map_mask_light = imfill(medfilt2(filled, [15,15]), 'holes');
+
+%% Resize
 tissue_mask_with_lepto = imresize(filtered_tissue_map_mask_light, [channel_x, channel_y]);
+
+%% Fill gaps
 tissue_mask = imfill(tissue_mask_with_lepto, 'holes');
 
 end
