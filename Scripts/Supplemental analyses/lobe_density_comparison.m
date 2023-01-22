25294872282f9d8dc4cb7c038b6c6ead40667984
CorinneAuger <corinneauger7@gmail.com>
2023-01-22 16:26:18 -0500

    Detect correct IA details column and change units

--- Scripts/Supplemental analyses/lobe_density_comparison.m
+++ Scripts/Supplemental analyses/lobe_density_comparison.m
@@ -26,24 +26,12 @@ for a = 1:3
     [n, ~] = size(data);
     
     %% Preallocate
-    object_density_by_lobe = NaN(71, 4);
+    object_density_by_lobe = NaN(26, 7);
     
-    for i = 1:n
-        %% Set up 4-column matrix with columns separated by lobe
-        if data(i, 2) == 1
-            lobe_pos = 1;
-        elseif data(i, 2) == 4
-            lobe_pos = 2;
-        elseif data(i, 2) == 5
-            lobe_pos = 3;
-        elseif data(i, 2) == 7
-            lobe_pos = 4;
-        end
-        
+    for i = 1:n      
         %% Get number of objects
         if strcmp(stain, 'Iron')
             objects_in_section = data(i, 6);
-            excel_column_letter = 'P';
         elseif strcmp(stain, 'GFAP')
             objects_in_section = data(i, 7);
             excel_column_letter = 'P';
@@ -58,32 +46,44 @@ for a = 1:3
         IA_details_sheet_name = sprintf('IA_details__CAA%d_%d_%s.xlsx', brain, block, stain);
         cd(directory.IA_details)
         
-        excel_column = sprintf('%s:%s', excel_column_letter, excel_column_letter);
-        
         if isfile(IA_details_sheet_name)
+            
+            % Figure out which excel column has area data
+            column_names = readtable(IA_details_sheet_name, 'Range', 'A1:Z1', 'ReadVariableNames', true);
+            excel_column_index = strfind(column_names.Properties.VariableNames, 'Area__m__');
+            excel_column_index(cellfun(@isempty, excel_column_index)) = {0};
+            excel_column_number = find(cell2mat(excel_column_index));
+            
+            % Format as letter
+            excel_column_letter = char(excel_column_number + 64);
+            excel_column = sprintf('%s:%s', excel_column_letter, excel_column_letter);
+            
             areas_to_sum = table2array(readtable(IA_details_sheet_name, 'Range', excel_column));
             area_in_um = nansum(areas_to_sum);
-            area_in_mm = area_in_um * 10^6;
+            area_in_mm = area_in_um * 10^-6;
 
             %% Calculate density
-            object_density_by_lobe(i, lobe_pos) = objects_in_section/area_in_mm;
+            object_density_by_lobe(brain, block) = objects_in_section/area_in_mm;
         end
         
-        clear lobe_pos objects_in_section excel_column_letter brain block IA_details_sheet_name areas_to_sum area_in_um area_in_mm
+        clear objects_in_section excel_column_letter brain block IA_details_sheet_name areas_to_sum area_in_um area_in_mm
     end
     
     %% Make figure
-    % Set up figure
-    figure
-    boxplot(object_density_by_lobe)
+    % Preallocate for boxplot, then delete columns
+    object_density_by_lobe_cortex_only = object_density_by_lobe; 
+    object_density_by_lobe_cortex_only(:, 6) = []; 
+    object_density_by_lobe_cortex_only(:, 3) = []; 
+    object_density_by_lobe_cortex_only(:, 2) = []; 
+    
+    boxplot(object_density_by_lobe_cortex_only)
     
     % Add labels
     xlabel('Lobe', 'FontSize', 20);
     xticks([1 2 3 4]);
     xticklabels({'Frontal', 'Temporal', 'Parietal', 'Occipital'});
     xlim([0.5 4.5]);
-    a = get(gca,'XTickLabel');
-    set(gca,'XTickLabel',a,'fontsize',18)
+    b = get(gca,'XTickLabel');
     
     ylabel('Objects per mm^2')
     
@@ -91,8 +91,8 @@ for a = 1:3
     hold on
     
     % Add scatter plot
-    for block = [1, 2, 3, 4]
-        scatter(block * ones(71,1), object_density_by_lobe(:,block), 'black', '.');
+    for chron_block = 1:4    
+        scatter(chron_block * ones(26,1), object_density_by_lobe_cortex_only(:, chron_block), 'black', '.');
         hold on
     end
     
@@ -102,6 +102,10 @@ for a = 1:3
     % Save matrix
     matrix_save_name = sprintf('%s_lobe_comparison', stain);
     save(matrix_save_name, 'object_density_by_lobe')
+    
+    % Save excel sheet
+    object_density_by_lobe_table = array2table(object_density_by_lobe);
+    writetable(object_density_by_lobe_table, 'Lobe_comparison.xlsx', 'Sheet', stain, 'WriteVariableNames', false)
     
     % Save figure
     figure_save_name = sprintf('%s_lobe_comparison_figure.png', stain);
