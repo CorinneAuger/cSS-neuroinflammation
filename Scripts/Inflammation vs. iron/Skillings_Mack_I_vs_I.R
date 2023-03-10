33ec33756d941955bb2d7cde0194f5f645dcef38
CorinneAuger <corinneauger7@gmail.com>
2023-03-10 12:19:42 -0500

    Try new Conover packages

--- Scripts/Inflammation vs. iron/Skillings_Mack_I_vs_I.R
+++ Scripts/Inflammation vs. iron/Skillings_Mack_I_vs_I.R
@@ -3,19 +3,35 @@
 ## Install packages (only have to do once ever)
 install.packages("R.matlab")
 install.packages("Skillings.Mack")
+
+# For Conover test option 1
 install.packages("conover.test")
+
+# For Conover test option 2
+install.packages("DescTools")
+
+# For Conover test option 3
+install.packages('PMCMRplus')
 
 ## Load packages (every time you restart R)
 library(R.matlab)
 library(Skillings.Mack)
+
+# For Conover test option 1
 library(conover.test)
+
+# For Conover test option 2
+library(DescTools)
+
+# For Conover test option 3
+library(PMCMRplus)
 
 ## Import data
 # For old data
-#data_list <- readMat('/Volumes/Corinne hard drive/cSS project/Saved data/One-pixel interval analysis/CD68/Composite/All_brains_CD68_iron_intervals.mat')
+data_list <- readMat('/Volumes/Corinne hard drive/cSS project/Saved data/One-pixel interval analysis/CD68/Composite/All_brains_CD68_iron_intervals.mat')
 
 # For new data
-data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Inflammation vs. Iron (E)/All_brains_CD68_iron_intervals.mat')
+#data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Inflammation vs. Iron (E)/All_brains_CD68_iron_intervals.mat')
 
 # Reformat
 transposed_data <- data_list[['all.means']]
@@ -48,4 +64,12 @@ Ski.Mack(data)
 ## Conover's test of multiple comparisons
 # Reformat data
 conover_data <- as.list(as.data.frame(transposed_data))
+
+# Conover test option 1
 conover.test(x = conover_data, method = 'hochberg')
+
+# Conover test option 2
+ConoverTest(conover_data, method = "BH")
+
+# Conover test option 3
+kwAllPairsConoverTest(conover_data, p.adjust.method = 'BH')
