9b0face25611299fdd8ee494d1f0fdb425b19de2
CorinneAuger <corinneauger7@gmail.com>
2023-03-17 19:57:22 -0400

    Change Conover to Dunn test

--- Scripts/Inflammation vs. iron/Skillings_Mack_I_vs_I.R
+++ Scripts/Inflammation vs. iron/Skillings_Mack_I_vs_I.R
@@ -3,35 +3,19 @@
 ## Install packages (only have to do once ever)
 install.packages("R.matlab")
 install.packages("Skillings.Mack")
-
-# For Conover test option 1
-install.packages("conover.test")
-
-# For Conover test option 2
-install.packages("DescTools")
-
-# For Conover test option 3
-install.packages('PMCMRplus')
+install.packages("dunn.test")
 
 ## Load packages (every time you restart R)
 library(R.matlab)
 library(Skillings.Mack)
-
-# For Conover test option 1
-library(conover.test)
-
-# For Conover test option 2
-library(DescTools)
-
-# For Conover test option 3
-library(PMCMRplus)
+library(dunn.test)
 
 ## Import data
 # For old data
-data_list <- readMat('/Volumes/Corinne hard drive/cSS project/Saved data/One-pixel interval analysis/CD68/Composite/All_brains_CD68_iron_intervals.mat')
+#data_list <- readMat('/Volumes/Corinne hard drive/cSS project/Saved data/One-pixel interval analysis/GFAP/Composite/All_brains_GFAP_iron_intervals.mat')
 
 # For new data
-#data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Inflammation vs. Iron (E)/All_brains_CD68_iron_intervals.mat')
+data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Inflammation vs. Iron (E)/All_brains_GFAP_iron_intervals.mat')
 
 # Reformat
 transposed_data <- data_list[['all.means']]
@@ -47,7 +31,7 @@ categories <- dimensions[1]
 # Remove in loop
 for(i in brains:1) {
   boolean_sum <- sum(is.na(data[, i]))
-  if (boolean_sum == 4 | boolean_sum == 3) {
+  if (boolean_sum == 4) {
     data <- data[, -i]
     }
 }
@@ -61,15 +45,9 @@ transposed_data <- t(data)
 ## Skillings-Mack test
 Ski.Mack(data)
 
-## Conover's test of multiple comparisons
+## Dunn test of multiple comparisons
+dunn.test(data, method = 'bh')
+
 # Reformat data
 conover_data <- as.list(as.data.frame(transposed_data))
 
-# Conover test option 1
-conover.test(x = conover_data, method = 'hochberg')
-
-# Conover test option 2
-ConoverTest(conover_data, method = "BH")
-
-# Conover test option 3
-kwAllPairsConoverTest(conover_data, p.adjust.method = 'BH')
