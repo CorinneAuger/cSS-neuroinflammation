af134cb21ab22c9d3792e764420d0573f8ca4edc
CorinneAuger <corinneauger7@gmail.com>
2023-04-14 11:56:49 -0400

    Start using Conover test

--- Scripts/Lobe density comparison/lobe_comp_stats.R
+++ Scripts/Lobe density comparison/lobe_comp_stats.R
@@ -11,55 +11,36 @@ library(Skillings.Mack)
 library(dunn.test)
 
 ## Import data
-data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Lobe comparison (E)/GFAP_lobe_comparison.mat')
+data_list <- readMat('/Volumes/Corinne hard drive/cSS project/ALL EXCLUDED/Lobe comparison (E)/Iron_lobe_comparison.mat')
 
 # Reformat
-transposed_data <- data_list[['object.density.by.lobe']]
-data <- t(transposed_data)
-rm(data_list)
-
-## Remove columns with no observations
+conover_data <- data_list[['object.density.by.lobe']]
+## Remove columns with no observations for Skillings-Mack
 # Set up parameters
-dimensions <- dim(data)
-brains <- dimensions[2]
-categories <- dimensions[1]
+dimensions <- dim(conover_data)
+brains <- dimensions[1]
+categories <- dimensions[2]
+SM_data <- conover_data
 
 # Remove in loop
 for(i in brains:1) {
-  boolean_sum <- sum(is.na(data[, i]))
-  if (boolean_sum == 4) {
-    data <- data[, -i]
+  boolean_sum <- sum(is.na(SM_data[i, ]))
+  if (boolean_sum == 4 || boolean_sum == 3) {
+    SM_data <- SM_data[-i,]
   }
 }
 
-# Change brain count and transposed data
-brains <- dim(data)[2]
-rm(dimensions)
-
-transposed_data <- t(data)
 
 ## Skillings-Mack test
-Ski.Mack(data)
+Ski.Mack(t(SM_data))
 
-## Remove incomplete brains
-transposed_dunn_data <- data
 
-for(i in brains:1) {
-  boolean_sum <- sum(is.na(transposed_dunn_data[, i]))
-  if (boolean_sum != 0) {
-    transposed_dunn_data <- transposed_dunn_data[, -i]
-  }
-}
+## Conover test
+# Make group vector
+col_names <- 1:dimensions[1]
+row_names <- 1:dimensions[2]
 
-dunn_data = t(transposed_dunn_data)
+iron_scores <- rep(row_names, each = dimensions[1])
 
-# Make group vector for Dunn test
-size <- dim(dunn_data)
-col_names <- 1:size[1]
-row_names <- 1:size[2]
-
-categories <- rep(row_names, each = size[1])
-
-## Dunn test
-dunn.test(x = c(dunn_data), g = categories, method = "bh", altp = TRUE) 
-
+# Conover test
+conover.test(x = c(conover_data), g = iron_scores, method = "hochberg", altp = TRUE) 
