b76b790f78e6e0ca43a522db6f30ada0a8e4cd1b
CorinneAuger <corinneauger7@gmail.com>
2023-06-11 16:47:48 -0400

    Make compatible with ADRC MTL scripts

--- Scripts/Heat map analysis/Aiforia_coregistration.m
+++ Scripts/Heat map analysis/Aiforia_coregistration.m
@@ -1,61 +1,47 @@
+function [rotation, D, tform, coregistered_stain, fixed, moving] = Aiforia_coregistration(cohort, directory, brain, block, fixed_image_stain, moving_image_stain)
+
 %% Coregisters NanoZoomer scans of two serial sections with different stains
 % For use in density_comparison.
 
-function [rotation, D, tform, coregistered_inflammation] = Aiforia_coregistration(brain, block, fixed_image_stain, moving_image_stain)
+% Arguments
+%   cohort: 'CAA' or 'ADRC'
+%   directory: struct of paths. Needs to include scripts, images_sizes_spreadsheets, and original_images.
+%   brain: number (ex. for CAA3_7_GFAP, brain = 3)
+%   block: number (ex. for CAA3_7_GFAP, block = 7)
+%   fixed_image_stain: name of stain to which the other stain will be coregistered, in quotes (ex. 'Iron')
+%   moving_image_stain: name of stain that will be coregistered to the other stain, in quotes (ex. 'GFAP')
 
-%% Input directory
-directory.original_images = '/Volumes/Corinne hard drive/cSS project/Original images';
-directory.image_sizes_spreadsheets = '/Users/corinneauger/Documents/Aiforia heatmap coregistration/Image sizes spreadsheets';
+%% Get slide names
+if strcmp(cohort, 'CAA')
+    fixed_slide_name = sprintf('CAA%d_%d_%s.png', brain, block, fixed_image_stain);
+    moving_slide_name = sprintf('CAA%d_%d_%s.png', brain, block, moving_image_stain);
+elseif strcmp(cohort, 'ADRC')
+    fixed_slide_name = sprintf('%d_%d_%s.png', brain, block, fixed_image_stain);
+    moving_slide_name = sprintf('%d_%d_%s.png', brain, block, moving_image_stain);
+end
 
 %% Import images and convert to gray
 cd(directory.original_images)
-fixed_3D = imread(sprintf('CAA%d_%d_%s.png', brain, block, fixed_image_stain));
+fixed_3D = imread(fixed_slide_name);
 unresized_fixed = rgb2gray(fixed_3D);
-moving_3D = imread(sprintf('CAA%d_%d_%s.png', brain, block, moving_image_stain));
+moving_3D = imread(moving_slide_name);
 moving = rgb2gray(moving_3D);
-%figure;
-%imshowpair(fixed,moving,'montage');
-%rotation = input('Rotate 180 degrees?   If no, reply 0; if yes, reply 1     ');
 
-%% Import spreadsheets with sizes of each image
-cd(directory.image_sizes_spreadsheets)
-image_size_sheet_name_moving = join(['Aiforia_image_sizes_', moving_image_stain, '.xlsx']);
-image_size_matrix_moving = xlsread(image_size_sheet_name_moving);
-image_size_sheet_name_fixed = join(['Aiforia_image_sizes_', fixed_image_stain, '.xlsx']);
-image_size_matrix_fixed = xlsread(image_size_sheet_name_fixed);
-% column 1 is width; column 2 is height; column 3 is rotation; column 4 is how many rois
-
-%% Look up whether sections need to be rotated to align in spreadsheet (make spreadsheet manually)
-if block == 1
-    row_number = ((brain - 1)*4) + 1;
-elseif block == 4
-    row_number = ((brain - 1)*4) + 2;
-elseif block == 5
-    row_number = ((brain - 1)*4) + 3;
-elseif block == 7
-    row_number = brain * 4;
-end
-
-rotation = image_size_matrix_moving(row_number, 3);
-
-%% Look up image sizes
-spreadsheet_width = image_size_matrix_fixed(row_number, 1);
-spreadsheet_height = image_size_matrix_fixed(row_number, 2);
+%% Look up image size and rotation
+cd(directory.scripts)
+[spreadsheet_width, spreadsheet_height, rotation, ~] = get_image_size_from_spreadsheet(cohort, directory, brain, block, moving_image_stain);
 
 %% Shrink images and resize to one another
-scaled_width = spreadsheet_width/14.5;
-scaled_height = spreadsheet_height/14.5;
-fixed = imresize(unresized_fixed, [scaled_height, scaled_width]);
-
-%% Rotate if needed
-if rotation == 1
-    moving = imrotate(moving, 180);
-elseif rotation == 2
-    moving = imrotate(moving, 90);
+if strcmp(cohort, 'CAA')
+    scaled_width = spreadsheet_width/14.5;
+    scaled_height = spreadsheet_height/14.5;
+    fixed = imresize(unresized_fixed, [scaled_height, scaled_width]);
 else
+    fixed = unresized_fixed;
 end
 
-close;
+% Rotate if needed
+moving = imrotate(moving, rotation);
 
 %% Set up imregtform function
 [moving_x, moving_y, ~] = size(moving);
@@ -66,29 +52,25 @@ Rfixed = imref2d([fixed_x, fixed_y]);
 
 %% Modify optimizer settings
 optimizer.GrowthFactor = 1.01;
+optimizer.Epsilon = 1.5e-6;
 optimizer.InitialRadius = 0.002;
 optimizer.MaximumIterations = 600;
-optimizer.Epsilon = 1.5e-6;
 
 %% Perform coregistration
 moving = imhistmatch(moving,fixed);
 tform = imregtform(moving, fixed, 'affine', optimizer, metric);
 
 %% Apply transformation to moving section
-affine_coreg_heat_map = imwarp(moving, Rmoving, tform, 'OutputView', Rfixed);
+affine_coreg_moving = imwarp(moving, Rmoving, tform, 'OutputView', Rfixed);
 
 %% Display coregistered image overlayed on fixed image
-%figure;
-%imshowpair(fixed,affine_coreg_heat_map);
+% figure;
+% imshowpair(fixed,affine_coreg_moving);
 
 %% Non-rigid transformation - perform after rigid transformation above to improve registration (note: this is very slow--7 mins per slide)
-
-[D,coregistered_inflammation] = imregdemons(affine_coreg_heat_map,fixed,[1000 400 400],'AccumulatedFieldSmoothing',3, 'DisplayWaitbar',true); % number of iterations at each pyramidal layer
+[D,coregistered_stain] = imregdemons(affine_coreg_moving,fixed,[1000 400 400],'AccumulatedFieldSmoothing',3, 'DisplayWaitbar',true); % number of iterations at each pyramidal layer
 figure;
-imshowpair(fixed,coregistered_inflammation);
+imshowpair(fixed,coregistered_stain);
 close all
-
-%filename = sprintf('CAA%d_%d_%s_iron-coregistered_heat_map.tif', brain, block, inflammatory_marker);
-%imwrite(squeeze(transformed_inflammation_heat_map), filename, 'WriteMode', 'append',  'Compression', 'none');
 
 end
